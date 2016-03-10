2ee587cae7cb7921c55ce6e14cf3af3de7d33620
Alexandes Heiets <sanomchik@gmail.com>
2016-03-10 10:15:16 +0200

    test

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
 
 
 //$("a[href*='#']").mPageScroll2id();
-
+$(function() {
+  $('#gallery-container').snapGallery();
+})
 
 });
+$(window).load(function() { // makes sure the whole site is loaded
+            $('.loader').fadeOut(); // will first fade out the loading animation
+            $('.loader_inner').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website.
+            $('body').delay(1000).css({'overflow':'visible'});
+        })
